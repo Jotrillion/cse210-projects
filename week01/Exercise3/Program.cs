f845716197c7e9f7f5491861c5ccee99fc00ca34
jotrillion <josaphatngandu1@gmail.com>
2025-09-09 07:21:24 +0200

    week1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string input = "";
+
+        int guess = -1;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        do
+        {
+
+            Console.Write("Enter a guess: ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != magicNumber);
+
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string input = "";
+        int number;
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+            number = int.Parse(input);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                int sum = 0;
+                foreach (int num in numbers)
+                {
+                    sum += num;
+                }
+                Console.WriteLine($"The sum is: {sum}");
+            }
+
+        } while (number != 0);
+        if (numbers.Count > 0)
+        {
+            
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Largest number: {max}");
+        }
+        else
+        {
+            Console.WriteLine("\nNo numbers were entered.");
+        }
     }
 }
