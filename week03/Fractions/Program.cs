fbf9e772dedb87b51780985b6de093500eef89c8
jotrillion <josaphatngandu1@gmail.com>
2025-09-23 15:52:42 +0200

    three week try

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
+
+
+
     static void Main(string[] args)
+
     {
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6, 7);
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
         Console.WriteLine("Hello World! This is the Fractions Project.");
     }
+    
+    
 }
